f6a3039e0c269eb334ef09d4517ad5f045c1a06b
Gurukasi <163010968+gurukasi-2006@users.noreply.github.com>
2025-10-11 14:51:58 +0530

    Update ExoCNN_Features.py
    changed the loading to first
    
    Signed-off-by: Gurukasi <163010968+gurukasi-2006@users.noreply.github.com>

--- pages/ExoCNN_Features.py
+++ pages/ExoCNN_Features.py
@@ -3,8 +3,9 @@ import base64
 import os
 from styling import add_advanced_loading_animation, load_custom_styling_back
 
-load_custom_styling_back()
+
 add_advanced_loading_animation()
+load_custom_styling_back()
 # --- Page Configuration ---
 st.set_page_config(
     page_title="Project Features",
@@ -281,6 +282,7 @@ feature_cards_html = """
 """
 
 st.markdown(feature_cards_html, unsafe_allow_html=True)
+
 
 
 
