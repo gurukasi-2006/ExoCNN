13be226b3b20bcd558d0b4ca9e1cbed5a1263f45
Gurukasi <163010968+gurukasi-2006@users.noreply.github.com>
2025-10-11 14:44:19 +0530

    Update FITS_Viewer.py
    Added the Loading animation and background for sidebar
    
    Signed-off-by: Gurukasi <163010968+gurukasi-2006@users.noreply.github.com>

--- pages/FITS_Viewer.py
+++ pages/FITS_Viewer.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import lightkurve as lk
 import base64
-from styling import add_advanced_loading_animation
+from styling import add_advanced_loading_animation, load_custom_styling_back
 
+load_custom_styling_back()
 add_advanced_loading_animation()
 # --- Page Config & Background ---
 st.set_page_config(layout="wide")
@@ -158,3 +159,4 @@ if uploaded_file is not None:
     except Exception as e:
 
         st.error(f"An error occurred while reading the FITS file: {e}")
+
